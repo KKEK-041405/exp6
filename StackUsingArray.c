46b60c3358e359ab284f8948a2c3fbad759fbd46
Konatham komal eshwara kumar <119280751+KKEK-041405@users.noreply.github.com>
2023-12-26 21:23:47 +0530

    Update StackUsingArray.c

--- StackUsingArray.c
+++ StackUsingArray.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define STACK_MAX_SIZE 10
-#include "StackOperations.c"
+void push(int);
+void pop();
+void display();
+void isEmpty();
+void peek();
 int main() {
 int op, x;
 while(1) {
@@ -30,4 +34,66 @@ case 6:
 exit(0);
 }
 }
+}
+
+#define size 10
+//define the top to -1
+
+int stack[size],top=-1,i;
+void push(int val){
+//write your code here to push an element
+if(top==size-1) {
+printf("Stack is overflow.");
+printf("\n");
+}
+else{
+top++;
+stack[top]=val;
+printf("Successfully pushed.\n");
+}
+}
+void display() {
+//Write your code here to display the stack
+int i;
+if(top==-1){
+printf("Stack is empty.\n");
+}
+else {
+printf("Elements of the stack are : ");
+for(i=top;i>=0;i--) {
+printf("%d ",stack[i]);
+}
+printf("\n");
+}
+}
+void pop() {
+//write your code here to pop an element
+if(top==-1) {
+printf("Stack is underflow.");
+printf("\n");
+}
+else{
+i=stack[top];
+top--;
+printf("Popped value = %d\n",i);
+}
+}
+void isEmpty() {
+//write your code here to check whether the stack is empty not
+if(top==-1){
+printf("Stack is empty.\n");
+}
+else
+{
+printf("Stack is not empty.\n");
+}
+}
+void peek(){
+   if(top==-1){
+printf("Stack is underflow.\n");
+}
+else{
+printf("Peek value = %d\n",stack[top]);
+}
+ 
 }
